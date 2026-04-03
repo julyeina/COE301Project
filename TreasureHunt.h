0af455209cf145435765d57f2a74f6ffdd612a32
julyeina <seoyoon5955@gmail.com>
2026-04-03 12:55:25 -0500

    Add files via upload

---
+++ TreasureHunt.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include "Clue.h"
+using namespace std;
+
+namespace utility{
+    class TreasureHunt{
+    private:
+        vector<string> mapGrid;
+        vector<Clue> clues;
+        int playerRow;
+        int playerCol;
+        int totalScore;
+        bool gameOver;
+
+    public:
+        TreasureHunt();
+        void loadMap(const string& map);
+        void loadClues(const string& clues);
+        void drawMap();
+        void movePlayer(char direction)
+        void triggerClue(char symbol);
+        void startGame();
+        void displayFinalResult();
+    };
+}
