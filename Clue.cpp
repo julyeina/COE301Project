59fba2d32ec91032f8e1ad56beea0859228a3a8b
julyeina <seoyoon5955@gmail.com>
2026-04-04 12:46:46 -0500

    Update Clue.cpp
    updated clue.cpp
    
    I added the additional functions and made the user anwer case-insentitive

--- Clue.cpp
+++ Clue.cpp
@@ -1,30 +1,64 @@
 #include "Clue.h"
+#include <cctype>
 
-Clue::Clue()
-	: symbol('?'),
-	maxAttempts(0),
-	points(0),
-	completed(false) {}
+using namespace std;
 
-Clue::Clue(char symbol, 
-		   const string& question, 
-	       const string& correctAnswer, 
-	       int maxAttempts, 
-	       int points)
-	: symbol(symbol),
-	question(question),
-	correctAnswer(correctAnswer),
-	maxAttempts(maxAttempts),
-	points(points),
-	completed(false) {}
+// make game case-insensitive for user answer
+static string toLowerCopy(const std::string& s) {
+	string out = s;
+	for (char& ch : out){
+		ch = static_cast<char>(std::tolower(static_cast<unsigned char>(ch)));
+	} 
+	return out;
+}
+
+//default constructor
+Clue::Clue(){
+	symbol = ' ';
+	question = "";
+	correctAnswer = "";
+	maxAttempts = 0;
+	points = 0;
+	completed = false;
+}
+//constructor
+Clue::Clue(char sym, const string& q, const string& answer, int attempts, int pts){
+	symbol = sym;
+	question = q;
+	correctAnswer = answer;
+	maxAttempts = attempts;
+	points = pts;
+	completed = false;
+}
+	
 char Clue::getSymbol() const {
+	
 	return symbol;
+}
+
+string Clue::getQuestion() const{
+	return question;
+}
+
+string Clue:: getCorrectAnswer() const{
+	return correctAnswer;
+}
+int Clue:: getMaxAttempts() const{
+	return maxAttempts;
+}
+int Clue::getPoints() const{
+	return points;
 }
 
 bool Clue::isCompleted() const {
 	return completed;
 }
 
-bool Clue::ask() {
-	return false;
+void Clue:: setCompleted(bool status){
+	completed = status;
 }
+
+bool Clue::checkAnswer(const string& userAnswer) const {
+	return toLowerCopy(userAnswer) == toLowerCopy(correctAnswer);
+}
+
